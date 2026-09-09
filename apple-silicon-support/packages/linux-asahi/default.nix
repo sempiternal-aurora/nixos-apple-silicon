3d49df6ad86095c0a44adf440acdb6b9275dd4f7
sempiternal-aurora <78790545+sempiternal-aurora@users.noreply.github.com>
2026-09-10 02:14:15 +1000

    linux-asahi: enable BRCM extension
    Allows for bluetooth audio without stuttering

--- apple-silicon-support/packages/linux-asahi/default.nix
+++ apple-silicon-support/packages/linux-asahi/default.nix
@@ -59,6 +59,9 @@ let
 
               APPLE_PMGR_MISC = yes;
               APPLE_PMGR_PWRSTATE = yes;
+
+              # Defaults to 'n', but needed for bt audio to not stutter
+              BT_BRCMEXT = yes;
             };
             features.rust = true;
           }
