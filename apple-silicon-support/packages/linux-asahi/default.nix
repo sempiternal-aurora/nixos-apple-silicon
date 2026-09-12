eaa15e800df596acfe6b74d4f26ddd847ca646ea
Florian Klink <flokli@flokli.de>
2026-09-12 17:11:28 +0300

    linux-asahi: asahi-7.1.13-1 -> asahi-7.1.13-3

--- apple-silicon-support/packages/linux-asahi/default.nix
+++ apple-silicon-support/packages/linux-asahi/default.nix
@@ -33,8 +33,8 @@ let
         src = fetchFromGitHub {
           owner = "AsahiLinux";
           repo = "linux";
-          tag = "asahi-7.1.13-1";
-          hash = "sha256-G1MQyuqwVN4uwm7awpdAOwxZZ9UNETGRr6Z3sQCbzgY=";
+          tag = "asahi-7.1.13-3";
+          hash = "sha256-quvdcQ2LbYQyCDQFKc6KPjWv+f5fpWfUrXo5Id0kpwE=";
         };
 
         kernelPatches = [
